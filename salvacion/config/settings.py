94cd63786e01e5b24bbf2bd02a06de6f07a37d44
dvkim28 <d.villarionovich@gmail.com>
2023-11-16 13:06:50 +0200

    release

--- salvacion/config/settings.py
+++ salvacion/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k32eh-*7!#!t+cp^1$jgd6ndx8%hcg9-*p#&+219+kozv#iecw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
