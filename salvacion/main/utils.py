3f9bda4f78838b48450c0fbe571d486472c849bf
dvkim28 <d.villarionovich@gmail.com>
2023-11-27 19:20:11 +0200

    multi language

--- salvacion/main/utils.py
+++ salvacion/main/utils.py
@@ -50,3 +50,18 @@ def send_telegram_bot(message):
         data = {'chat_id': chat_id, 'text': message}
         response = requests.post(url, data=data)
         return response.ok
+
+
+from django import template
+
+register = template.Library()
+
+LANGUAGE_TO_FLAG_MAP = {
+    'uk': 'ua',  # Украинский
+    'ru': 'ru',  # Русский
+    'es': 'es',  # Испанский
+}
+
+@register.filter
+def language_to_flag(lang_code):
+    return LANGUAGE_TO_FLAG_MAP.get(lang_code, 'us')  # 'us' - флаг по умолчанию
